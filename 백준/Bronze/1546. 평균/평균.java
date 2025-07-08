904063d47ad6ce36baddbce9a4fa1f3a185c4e32
우진 <hoshino.whale0102@gmail.com>
2025-07-09 08:42:30 +0900

    [Bronze I] Title: 평균, Time: 292 ms, Memory: 25564 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        double[] scores = new double[N];
+
+        double max = 0;
+        for (int i = 0; i < N; i++) {
+            scores[i] = sc.nextDouble();
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
+            scores[i] = scores[i] / max * 100;
+            sum += scores[i];
+        }
+
+        double avg = sum / N;
+        System.out.println(avg);
+    }
+}
