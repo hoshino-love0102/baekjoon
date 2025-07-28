cf5278fc3d1587386adbbf4362738badd513b54f
우진 <hoshino.whale0102@gmail.com>
2025-07-28 15:01:51 +0900

    [Bronze IV] Title: 상근날드, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,10 @@
+burger1 = int(input())
+burger2 = int(input())
+burger3 = int(input())
+drink1 = int(input())
+drink2 = int(input())
+sb = min(burger1, burger2, burger3)
+sd = min(drink1, drink2)
+n = sb + sd - 50
+
+print(n)
