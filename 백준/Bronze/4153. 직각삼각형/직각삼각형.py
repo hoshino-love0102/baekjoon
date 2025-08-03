43d7d22c043ce388af9d965783248e46f9a9169f
우진 <hoshino.whale0102@gmail.com>
2025-08-03 21:15:10 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,9 @@
+while True:
+    s = list(map(int, input().split()))
+    if s == [0, 0, 0]:
+        break
+    s.sort()
+    if s[0]**2 + s[1]**2 == s[2]**2:
+        print("right")
+    else:
+        print("wrong")
